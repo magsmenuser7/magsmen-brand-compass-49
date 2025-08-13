5ce0d55e7818de3c0e1dad5870e396f5ea79a678
magsmenuser7 <magsmenuser7@gmail.com>
2025-08-13 17:09:40 +0530

    script code changes

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "development" ? "/magsmen-brand-compass-49/" : "/",
+  base: "/magsmen-brand-compass-49/",
   server: {
     host: "::",
     port: 8080,
