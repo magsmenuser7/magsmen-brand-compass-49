b14d2128dddd9e1d085280bf515541dd2e5905e4
magsmenuser7 <magsmenuser7@gmail.com>
2025-08-13 16:21:26 +0530

    git hub pages added

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/magsmen-brand-compass-49/",
   server: {
     host: "::",
     port: 8080,
