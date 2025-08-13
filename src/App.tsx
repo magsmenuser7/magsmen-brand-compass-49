78c211bf84a97251783851c0833d53d09f636244
magsmenuser7 <magsmenuser7@gmail.com>
2025-08-13 18:00:29 +0530

    changes done

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/magsmen-brand-compass-49">
+      <BrowserRouter basename={import.meta.env.DEV ? "/magsmen-brand-compass-49" : "/"}>
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
